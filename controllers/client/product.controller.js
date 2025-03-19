dab988b2e454ee79fa2085515e02396fa260b36b
locht1 <locht1@runsystem.net>
2025-03-19 14:59:10 +0700

    Fix code convention

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,54 +1,49 @@
-const Product_Service = require('../../services/client/product.services')
+const productService = require('../../services/client/product.services')
 
 // [GET] /products
 // Get products
-module.exports.Index =  (req, res) => { 
-  var Filter_data = {
-    deleted: false
-    title: "name"
-    type: "desc",
+module.exports.index = async (req, res) => {
+  const filterData = {
+    deleted: false,
+    title: req.query.title || "name",
+    type: req.query.type || "desc",
     numItems: 4,
-  }
-  if (req.query.Title) { 
-    Filter_data.Title = req.query.Title
-  }
-  if (req.query.type) {
-    Filter_data.Type = req.query.type 
-  }
+  };
+  
   try {
-    const products = productService.getProducts(Filter_data)
+    const products = await productService.getProducts(filterData);
 
-    if (products.length == 0) { 
-      res.status(404).json({ message: "No products found" }) 
+    if (products.length === 0) {
+      return res.status(404).json({ message: "No products found" });
     }
 
-    return res.status(200).json(products) 
-  } catch (Err) { 
-    Console.log(Err) 
-    res.status(500).json({ message: "Error fetching products" })
+    return res.status(200).json(products);
+  } catch (err) { 
+    console.log(err);
+    return res.status(500).json({ message: "Error fetching products" });
   }
 }
 
 // [POST] /products
 // Create new product
-module.exports.NewProduct = async function (req, res) { 
-  let data_product = { 
-    Name: req.body.name, 
-    Price: req.body.price,
+module.exports.newProduct = async function (req, res) { 
+  const dataProduct = { 
+    name: req.body.name, 
+    price: req.body.price, 
     description: req.body.description,
     image: req.body.image,
     category: req.body.category
+  };
+
+  if (!dataProduct.name || dataProduct.price === undefined) { 
+    return res.status(400).json({ message: "Invalid data" });
   }
 
-  if (!data_product.Name || data_product.Price == undefined) { 
-    return res.status(400).json({ message: "Invalid data" })
-  }
+  const product = await productService.newProduct(dataProduct);
 
-  let product = await Product_Service.New_Product(data_product) 
-
-  if (product != null) { 
-    res.status(201).json({ message: "Product created successfully", product }) 
+  if (product) {
+    return res.status(201).json({ message: "Product created successfully", product });
   } else {
-    return res.status(500).json({ message: "Error creating product" })
+    return res.status(500).json({ message: "Error creating product" });
   }
 }
